b017346ab4faeb71ecce62088204d7b03b7359e4
lyyyuna <lyyyuna@outlook.com>
2020-06-12 18:00:24 +0800

    remove unnecessary lines

--- cmd/commonflags.go
+++ cmd/commonflags.go
@@ -6,38 +6,35 @@ import (
 )
 
 var (
-	target string
-	center string
-	mode   string
-	debugGoc bool
+	target     string
+	center     string
+	mode       string
+	debugGoc   bool
 	buildFlags string
-	packages string
-	appArgs string
+	packages   string
+	appArgs    string
 )
 
 // addBasicFlags adds a
-func addBasicFlags(cmdset *pflag.FlagSet) *pflag.FlagSet {
+func addBasicFlags(cmdset *pflag.FlagSet) {
 	cmdset.StringVar(&center, "center", "http://127.0.0.1:7777", "cover profile host center")
 	// bind to viper
 	viper.BindPFlags(cmdset)
-	return cmdset
 }
 
-func addCommonFlags(cmdset *pflag.FlagSet) *pflag.FlagSet {
+func addCommonFlags(cmdset *pflag.FlagSet) {
 	addBasicFlags(cmdset)
 	cmdset.StringVar(&mode, "mode", "count", "coverage mode: set, count, atomic")
-	cmdset.StringVar(&buildFlags, "buildflags", "",  "specify the build flags")
+	cmdset.StringVar(&buildFlags, "buildflags", "", "specify the build flags")
 	// bind to viper
 	viper.BindPFlags(cmdset)
-	return cmdset
 }
 
-func addBuildFlags(cmdset *pflag.FlagSet) *pflag.FlagSet {
+func addBuildFlags(cmdset *pflag.FlagSet) {
 	addCommonFlags(cmdset)
 	cmdset.StringVar(&packages, "packages", ".", "specify the package name, only . and ./... are supported")
 	// bind to viper
 	viper.BindPFlags(cmdset)
-	return cmdset
 }
 
 func addRunFlags(cmdset *pflag.FlagSet) {
