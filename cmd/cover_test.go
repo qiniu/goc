2b6d368b7e8e0439164ce8771e995c279c63cc5b
RryLee <rrylee1994@gmail.com>
2021-05-28 13:41:42 +0800

    fix: cover test use tmp dir

--- cmd/cover_test.go
+++ cmd/cover_test.go
@@ -8,12 +8,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var mainContent = []byte(`package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+}`)
+
+var goModContent = []byte(`module example.com/simple-project
+
+go 1.11
+`)
+
 func TestCoverSuccess(t *testing.T) {
-	workingDir := filepath.Join(baseDir, "../tests/samples/simple_project")
+	workingDir := filepath.Join(baseDir, "../tests/samples/tmp/project")
+	err := os.MkdirAll(workingDir, os.ModePerm)
+	assert.NoError(t, err)
+	defer os.RemoveAll(workingDir)
+
+	os.WriteFile(workingDir+"/main.go", mainContent, 0644)
+	os.WriteFile(workingDir+"/go.mod", goModContent, 0644)
 	os.Setenv("GO111MODULE", "on")
 
 	runCover(workingDir)
 
-	_, err := os.Lstat(workingDir + "/http_cover_apis_auto_generated.go")
+	_, err = os.Lstat(workingDir + "/http_cover_apis_auto_generated.go")
 	assert.Equal(t, err, nil, "the generate file should be generated.")
 }
