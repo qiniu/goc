d48a8c0b1057b71b493e436b0596eccc686ffed4
lyyyuna <lyyyuna@outlook.com>
2021-09-02 17:48:11 +0800

    update license

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,10 +1,22 @@
+/*
+ Copyright 2021 Qiniu Cloud (qiniu.com)
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
 package client
 
 import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"golang.org/x/term"
 	"io"
 	"io/ioutil"
 	"net"
@@ -12,6 +24,8 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+
+	"golang.org/x/term"
 
 	"github.com/olekukonko/tablewriter"
 	"github.com/qiniu/goc/v2/pkg/log"
